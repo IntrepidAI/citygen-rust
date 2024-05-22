f6bf5b8520a199652a30c5971e097eebd4331f0c
Alex Kocharin <alex@kocharin.ru>
2024-05-22 10:13:39 +0400

    city generation draft

---
+++ viewer/src/city_gen.rs
@@ -0,0 +1,466 @@
+// use bevy::{gizmos::gizmos::Gizmos, math::Vec2, render::color::Color};
+// use slotmap::{new_key_type, SlotMap};
+
+// new_key_type! { pub struct NodeId; }
+// new_key_type! { pub struct WayId; }
+
+// struct Node {
+//     position: Vec2,
+//     ways: Vec<WayId>,
+// }
+
+// struct Way {
+//     nodes: [NodeId; 2],
+//     way_type: WayType,
+// }
+
+// pub enum WayType {
+//     Highway,
+//     Normal,
+// }
+
+// struct RoadNetwork {
+//     nodes: SlotMap<NodeId, Node>,
+//     ways: SlotMap<WayId, Way>,
+// }
+
+// impl RoadNetwork {
+//     pub fn generate() -> Self {
+//         let mut nodes = SloatMap::new();
+//         let mut ways = SlotMap::new();
+
+//         let node1 = nodes.insert(Node { position: Vec2::new(0.0, 0.0), ways: Vec::new() });
+
+//         let root_way =
+//     }
+
+//     pub fn add_node(&mut self, position: Vec2) -> NodeId {
+//         self.nodes.insert(Node { position, ways: Vec::new() })
+//     }
+
+//     pub fn add_way(&mut self, start: NodeId, end: NodeId, way_type: WayType) -> WayId {
+//         let way_id = self.ways.insert(Way { nodes: [start, end], way_type });
+
+//         let start_node = self.nodes.get_mut(start).unwrap();
+//         let end_node = self.nodes.get_mut(end).unwrap();
+
+//         start_node.ways.push(way_id);
+//         end_node.ways.push(way_id);
+//     }
+
+//     pub fn draw(&self, gizmos: &mut Gizmos) {
+//         for way in self.ways.values() {
+//             let [start, end] = way.nodes;
+
+//             let start = self.nodes[start].position;
+//             let end = self.nodes[end].position;
+
+//             if way.is_highway {
+//                 gizmos.line_2d(start, end, Color::YELLOW);
+//             } else {
+//                 gizmos.line_2d(start, end, Color::WHITE);
+//             }
+//         }
+//     }
+// }
+
+use std::collections::BinaryHeap;
+
+use bevy::{gizmos::gizmos::Gizmos, math::Vec2, render::color::Color};
+use nanorand::Rng;
+use noise::core::open_simplex::open_simplex_2d;
+use petgraph::{graph::NodeIndex, Undirected};
+use rstar::primitives::GeomWithData;
+
+// const SEGMENT_COUNT_LIMIT: usize = 20;
+const BRANCH_ANGLE_DEVIATION: f32 = 3.0f32 * std::f32::consts::PI / 180.0f32;
+const STRAIGHT_ANGLE_DEVIATION: f32 = 15.0f32 * std::f32::consts::PI / 180.0f32;
+const MINIMUM_INTERSECTION_DEVIATION: f32 = 30.0f32 * std::f32::consts::PI / 180.0f32;
+const NORMAL_SEGMENT_LENGTH: f32 = 300.;
+const HIGHWAY_SEGMENT_LENGTH: f32 = 400.;
+const NORMAL_BRANCH_PROBABILITY: f32 = 0.4;
+const HIGHWAY_BRANCH_PROBABILITY: f32 = 0.05;
+const NORMAL_BRANCH_POPULATION_THRESHOLD: f32 = 0.;
+const HIGHWAY_BRANCH_POPULATION_THRESHOLD: f32 = 0.;
+const NORMAL_BRANCH_TIME_DELAY_FROM_HIGHWAY: u32 = 5;
+const MAX_SNAP_DISTANCE: f32 = 50.;
+
+pub struct NodeInfo {
+    pub position: Vec2,
+    pub can_snap: bool,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum WayType {
+    Highway,
+    Normal,
+}
+
+pub struct RoadNetwork {
+    pub graph: petgraph::Graph<NodeInfo, WayType, Undirected>,
+    pub tree: rstar::RTree<GeomWithData<[f32; 2], NodeIndex>>,
+}
+
+struct CheckTask {
+    source: petgraph::graph::NodeIndex,
+    target: Vec2,
+    way_type: WayType,
+    priority: u32,
+}
+
+impl PartialEq for CheckTask {
+    fn eq(&self, other: &Self) -> bool {
+        self.priority == other.priority
+    }
+}
+
+impl Eq for CheckTask {}
+
+impl PartialOrd for CheckTask {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for CheckTask {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.priority.cmp(&self.priority)
+    }
+}
+
+pub fn generate_segments(segment_count_limit: usize) -> RoadNetwork {
+    let mut rand = nanorand::WyRand::new_seed(1);
+    let population = noise::permutationtable::PermutationTable::new(1);//rand.generate());
+    let mut graph = petgraph::Graph::new_undirected();
+    let mut tree = rstar::RTree::new();
+
+    let root_node = graph.add_node(NodeInfo { position: Vec2::new(0.0, 0.0), can_snap: true });
+    tree.insert(GeomWithData::new([0.0, 0.0], root_node));
+
+    let mut queue = BinaryHeap::new();
+
+    queue.push(CheckTask {
+        source: root_node,
+        target: Vec2::new(HIGHWAY_SEGMENT_LENGTH, 0.),
+        way_type: WayType::Highway,
+        priority: 0,
+    });
+    queue.push(CheckTask {
+        source: root_node,
+        target: Vec2::new(-HIGHWAY_SEGMENT_LENGTH, 0.),
+        way_type: WayType::Highway,
+        priority: 0,
+    });
+
+    while let Some(mut task) = queue.pop() {
+        if graph.edge_count() > segment_count_limit {
+            break;
+        }
+
+        let target_index;
+        let old_target = task.target;
+
+        if let Some((new_target, new_index)) = local_constraints_apply(
+            graph[task.source].position,
+            task.target,
+            &graph,
+            &tree,
+        ) {
+            task.target = new_target;
+            target_index = new_index;
+        } else {
+            continue;
+        }
+
+        let target_index = if let Some(target_index) = target_index {
+            if graph.find_edge(task.source, target_index).is_some() {
+                continue;
+            }
+
+            target_index
+        } else {
+            graph.add_node(NodeInfo { position: task.target, can_snap: true })
+        };
+
+        tree.insert(GeomWithData::new([task.target.x, task.target.y], target_index));
+        graph.add_edge(task.source, target_index, task.way_type);
+
+        // {
+        //     graph[task.source].can_snap = true;
+
+        //     let mut iter = graph.edges(task.source).fuse();
+        //     let e1 = iter.next();
+        //     let e2 = iter.next();
+        //     let e3 = iter.next();
+
+        //     if let (Some(e1), Some(e2), None) = (e1, e2, e3) {
+        //         if e1.weight() == e2.weight() {
+        //             let p0 = graph[e1.source()].position;
+        //             let p1 = graph[e1.target()].position;
+        //             let p2 = graph[e2.source()].position;
+        //             let p3 = graph[e2.target()].position;
+
+        //             if ((p1-p0).normalize_or_zero()).dot((p3-p2).normalize_or_zero()) < 0.1 {
+        //                 graph[task.source].can_snap = false;
+        //             }
+        //         }
+        //     }
+        // }
+
+        if old_target != task.target {
+            continue;
+        }
+
+        for mut new_task in global_goals_generate(&mut rand, &graph, &population, task.source, target_index) {
+            new_task.priority += 1 + task.priority;
+            queue.push(new_task);
+        }
+    }
+
+    RoadNetwork { graph, tree }
+}
+
+pub fn draw_segments(
+    gizmos: &mut Gizmos,
+    network: &RoadNetwork,
+) {
+    for node in network.graph.node_indices() {
+        let position = network.graph[node].position;
+        let color = if network.graph[node].can_snap { Color::GREEN } else { Color::RED };
+        gizmos.circle_2d(position, 10., color);
+        gizmos.circle_2d(position, 9., color);
+        gizmos.circle_2d(position, 8., color);
+    }
+
+    for edge in network.graph.edge_indices() {
+        let (start, end) = network.graph.edge_endpoints(edge).unwrap();
+        let start = network.graph[start].position;
+        let end = network.graph[end].position;
+
+        match network.graph[edge] {
+            WayType::Highway => gizmos.line_2d(start, end, Color::YELLOW),
+            WayType::Normal => gizmos.line_2d(start, end, Color::GRAY),
+        }
+    }
+}
+
+pub fn local_constraints_apply(
+    source: Vec2,
+    target: Vec2,
+    graph: &petgraph::Graph<NodeInfo, WayType, Undirected>,
+    tree: &rstar::RTree<GeomWithData<[f32; 2], NodeIndex>>,
+) -> Option<(Vec2, Option<NodeIndex>)> {
+    let p1 = source;
+    let p2 = target;
+    // dbg!("-----------------------");
+    // dbg!(("start with", p1, p2));
+
+    if p1.distance_squared(p2) <= MAX_SNAP_DISTANCE * MAX_SNAP_DISTANCE {
+        return None;
+    }
+
+    let p2_extended = p2 + (p2 - p1).normalize_or_zero() * MAX_SNAP_DISTANCE;
+    let pmin = p1.min(p2_extended) - HIGHWAY_SEGMENT_LENGTH * Vec2::ONE;
+    let pmax = p1.max(p2_extended) + HIGHWAY_SEGMENT_LENGTH * Vec2::ONE;
+
+    let envelope = rstar::AABB::from_corners(pmin.into(), pmax.into());
+    let mut new_target = None;
+    let mut new_target_dist = f32::MAX;
+
+    for point in tree.locate_in_envelope(&envelope) {
+        let node1_position = graph[point.data].position;
+        if node1_position == p1 { continue; }
+
+        for other in graph.neighbors(point.data) {
+            let node2_position = graph[other].position;
+            if node2_position == p1 { continue; }
+
+            let is = segment_intersection(p1, p2_extended, node1_position, node2_position);
+
+            if let Some(is) = is {
+                let dist = p1.distance_squared(is);
+                if new_target.is_none() || dist < new_target_dist {
+                    // dbg!("found better", is, dist);
+                    new_target = Some((is, (node1_position, node2_position)));
+                    new_target_dist = dist;
+                }
+            }
+        }
+    }
+
+    let p2 = if let Some((is, (node1_position, node2_position))) = new_target {
+        let angle = (is - p1).angle_between(node2_position - node1_position).abs();
+
+        if angle < MINIMUM_INTERSECTION_DEVIATION || angle > std::f32::consts::PI - MINIMUM_INTERSECTION_DEVIATION {
+            return None;
+        }
+
+        is
+    } else {
+        p2
+    };
+
+    let pmin = p1.min(p2) - MAX_SNAP_DISTANCE * Vec2::ONE;
+    let pmax = p1.max(p2) + MAX_SNAP_DISTANCE * Vec2::ONE;
+    let envelope = rstar::AABB::from_corners(pmin.into(), pmax.into());
+
+    let mut new_target = None;
+    let mut new_target_dist = f32::MAX;
+
+    for point in tree.locate_in_envelope(&envelope) {
+        if !graph[point.data].can_snap {
+            continue;
+        }
+
+        if *point.geom() == [p1.x, p1.y] {
+            continue;
+        }
+
+        let node1_position = graph[point.data].position;
+
+        let dist = segment_point_distance_squared(p1, p2, node1_position);
+        if dist <= MAX_SNAP_DISTANCE * MAX_SNAP_DISTANCE {
+            let dist = p1.distance_squared(node1_position);
+            if dist < new_target_dist {
+                // dbg!(("found snap", node1_position, dist));
+                new_target = Some((point.data, node1_position));
+                new_target_dist = dist;
+            }
+        }
+    }
+
+    let p2 = new_target.map(|(_, pos)| pos).unwrap_or(p2);
+    let node_index = new_target.map(|(idx, _)| idx);
+
+    if p2.distance_squared(p1) > MAX_SNAP_DISTANCE * MAX_SNAP_DISTANCE {
+        Some((p2, node_index))
+    } else {
+        None
+    }
+}
+
+fn global_goals_generate(
+    rand: &mut nanorand::WyRand,
+    graph: &petgraph::Graph<NodeInfo, WayType, Undirected>,
+    population: &noise::permutationtable::PermutationTable,
+    prev_source_idx: petgraph::graph::NodeIndex,
+    prev_target_idx: petgraph::graph::NodeIndex,
+) -> Vec<CheckTask> {
+    let mut tasks = Vec::new();
+
+    let prev_source = graph[prev_source_idx].position;
+    let prev_target = graph[prev_target_idx].position;
+// dbg!((prev_source, prev_target, prev_source_idx, prev_target_idx));
+
+    let prev_type = graph.edges_connecting(prev_source_idx, prev_target_idx).next().unwrap().weight();
+
+    let continue_straight = prev_target + (prev_target - prev_source).normalize_or_zero() * if *prev_type == WayType::Highway {
+        HIGHWAY_SEGMENT_LENGTH
+    } else {
+        NORMAL_SEGMENT_LENGTH
+    };
+    let straight_pop = sample_population(population, prev_target);
+
+    if *prev_type == WayType::Highway {
+        let random_straight = prev_target + (prev_target - prev_source).normalize_or_zero().rotate(
+            Vec2::from_angle(rand.generate::<f32>() * STRAIGHT_ANGLE_DEVIATION * 2. - STRAIGHT_ANGLE_DEVIATION)
+        ) * HIGHWAY_SEGMENT_LENGTH;
+        let random_pop = sample_population(population, random_straight);
+        let road_pop = if random_pop > straight_pop {
+            tasks.push(CheckTask {
+                source: prev_target_idx,
+                target: random_straight,
+                way_type: *prev_type,
+                priority: 0,
+            });
+            random_pop
+        } else {
+            tasks.push(CheckTask {
+                source: prev_target_idx,
+                target: continue_straight,
+                way_type: *prev_type,
+                priority: 0,
+            });
+            straight_pop
+        };
+
+        if road_pop > HIGHWAY_BRANCH_POPULATION_THRESHOLD {
+            for direction in [-1, 1] {
+                if rand.generate::<f32>() >= HIGHWAY_BRANCH_PROBABILITY { continue; }
+
+                let random_angle = rand.generate::<f32>() * BRANCH_ANGLE_DEVIATION * 2. - BRANCH_ANGLE_DEVIATION;
+                let branch = prev_target + (prev_target - prev_source).normalize_or_zero().rotate(
+                    Vec2::from_angle(direction as f32 * std::f32::consts::FRAC_PI_2 + random_angle)
+                ) * HIGHWAY_SEGMENT_LENGTH;
+
+                tasks.push(CheckTask {
+                    source: prev_target_idx,
+                    target: branch,
+                    way_type: WayType::Highway,
+                    priority: 0,
+                });
+            }
+        }
+    } else if straight_pop > NORMAL_BRANCH_POPULATION_THRESHOLD {
+        tasks.push(CheckTask {
+            source: prev_target_idx,
+            target: continue_straight,
+            way_type: *prev_type,
+            priority: 0,
+        });
+    }
+
+    if straight_pop > NORMAL_BRANCH_POPULATION_THRESHOLD {
+        for direction in [-1, 1] {
+            if rand.generate::<f32>() >= NORMAL_BRANCH_PROBABILITY { continue; }
+
+            let random_angle = rand.generate::<f32>() * BRANCH_ANGLE_DEVIATION * 2. - BRANCH_ANGLE_DEVIATION;
+            let branch = prev_target + (prev_target - prev_source).normalize_or_zero().rotate(
+                Vec2::from_angle(direction as f32 * std::f32::consts::FRAC_PI_2 + random_angle)
+            ) * NORMAL_SEGMENT_LENGTH;
+
+            tasks.push(CheckTask {
+                source: prev_target_idx,
+                target: branch,
+                way_type: WayType::Normal,
+                priority: if *prev_type == WayType::Highway {
+                    NORMAL_BRANCH_TIME_DELAY_FROM_HIGHWAY
+                } else {
+                    0
+                },
+            });
+        }
+    }
+
+    tasks
+}
+
+pub fn sample_population(population: &noise::permutationtable::PermutationTable, at: Vec2) -> f32 {
+    let at: [f64; 2] = (at.as_dvec2() / 102400. * 15.).into();
+    open_simplex_2d(at.into(), population) as f32
+}
+
+fn segment_intersection(p0: Vec2, p1: Vec2, p2: Vec2, p3: Vec2) -> Option<Vec2> {
+    // https://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
+    let s1 = p1 - p0;
+    let s2 = p3 - p2;
+
+    let s = (-s1.y * (p0.x - p2.x) + s1.x * (p0.y - p2.y)) / (-s2.x * s1.y + s1.x * s2.y);
+    let t = ( s2.x * (p0.y - p2.y) - s2.y * (p0.x - p2.x)) / (-s2.x * s1.y + s1.x * s2.y);
+
+    if (0.0..=1.0).contains(&s) && (0.0..=1.0).contains(&t) {
+        Some(Vec2::new(p0.x + (t * s1.x), p0.y + (t * s1.y)))
+    } else {
+        None
+    }
+}
+
+fn segment_point_distance_squared(p0: Vec2, p1: Vec2, point: Vec2) -> f32 {
+    let l2 = (p0 - p1).length_squared();
+    if l2 == 0. {
+        return (point - p0).length_squared();
+    }
+
+    let t = ((point - p0).dot(p1 - p0) / l2).clamp(0., 1.);
+    (point - (p0 + t * (p1 - p0))).length_squared()
+}
--- viewer/src/main.rs
+++ viewer/src/main.rs
@@ -1,19 +1,34 @@
 #![allow(clippy::too_many_arguments)]
-use std::time::Duration;
+use std::net;
+use std::time::{Duration, SystemTime};
 
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::gltf::GltfExtras;
 use bevy::input::common_conditions::input_toggle_active;
 use bevy::prelude::*;
+use bevy::render::render_asset::RenderAssetUsages;
+use bevy::render::render_resource::{Extent3d, TextureDimension, TextureFormat};
 use bevy::window::PrimaryWindow;
 use bevy_inspector_egui::bevy_egui::EguiContexts;
 use bevy_mod_raycast::prelude::*;
+use noise::core::open_simplex::open_simplex_2d;
+use noise::permutationtable::PermutationTable;
 
 mod asset_reader;
+mod city_gen;
 mod orbit_camera;
 
 #[derive(Resource)]
 struct MapFile(String);
+
+#[derive(Resource)]
+struct RoadNetwork(crate::city_gen::RoadNetwork);
+
+impl RoadNetwork {
+    fn new(segment_count_limit: usize) -> Self {
+        Self(city_gen::generate_segments(segment_count_limit))
+    }
+}
 
 fn main() {
     let file = std::env::args().nth(1).unwrap_or_default();
@@ -39,6 +54,8 @@ fn main() {
         ))
         .add_systems(Startup, (spawn_environment, spawn_scene))
         .add_systems(Update, mouse_move)
+        .add_systems(Update, show_gizmos)
+        .add_systems(Update, ui)
         .insert_resource(ClearColor(Color::rgb(0., 0., 0.)))
         .insert_resource(AmbientLight {
             color: Color::WHITE,
@@ -46,19 +63,84 @@ fn main() {
         })
         .insert_resource(Msaa::default())
         .insert_resource(MapFile(file))
+        .insert_resource(RoadNetwork::new(1))
         .init_resource::<Highlighted>()
         .run();
 }
 
+fn ui(
+    mut commands: Commands,
+    mut contexts: EguiContexts,
+    mut limit: Local<usize>,
+    mut limit_txt: Local<String>,
+) {
+    let ctx = contexts.ctx_mut();
+
+    bevy_inspector_egui::egui::Window::new("")
+        .title_bar(false)
+        .auto_sized()
+        .show(ctx, |ui| {
+            if ui.text_edit_singleline(&mut *limit_txt).changed() {
+                if let Ok(value) = limit_txt.parse() {
+                    *limit = value;
+                }
+            }
+
+            if ui.button("regenerate").clicked() {
+                *limit += 1;
+                *limit_txt = limit.to_string();
+                commands.insert_resource(RoadNetwork::new(*limit));
+            }
+        });
+}
+
+fn show_gizmos(
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    camera_query: Query<(&Camera, &GlobalTransform)>,
+    mut gizmos: Gizmos,
+    network: Res<RoadNetwork>,
+    mut contexts: EguiContexts,
+) {
+    crate::city_gen::draw_segments(&mut gizmos, &network.0);
+
+    let intersect = (|| {
+        let window = window_query.get_single().ok()?;
+        let (camera, gxform) = camera_query.get_single().ok()?;
+        let cursor_position = window.cursor_position()?;
+        let cursor_ray = camera.viewport_to_world(gxform, cursor_position)?;
+        cursor_ray.intersect_plane(Vec3::ZERO, Plane3d::new(Vec3::Z))
+            .map(|pos| cursor_ray.origin + cursor_ray.direction * pos)
+    })();
+
+    // if let Some(intersect) = intersect {
+    //     let p1 = Vec2::new(10000., 10000.);
+    //     let p2 = intersect.xy();
+    //     if let Some((p2, _)) = city_gen::local_constraints_apply(p1, p2, &network.0.graph, &network.0.tree) {
+    //         gizmos.line_2d(Vec2::new(10000., 10000.), p2, Color::RED);
+    //     }
+    // }
+
+    let ctx = contexts.ctx_mut();
+
+    bevy_inspector_egui::egui::Window::new("x")
+        .title_bar(false)
+        .auto_sized()
+        .show(ctx, |ui| {
+            ui.label(format!("{intersect:?}"));
+            if let Some(intersect) = intersect {
+                let intersect = intersect.truncate();
+                let population = noise::permutationtable::PermutationTable::new(1);//rand.generate());
+                ui.label(format!("{:?}", city_gen::sample_population(&population, intersect)));
+            }
+        });
+}
+
 fn spawn_environment(mut commands: Commands) {
-    let camera_transform = Transform::from_xyz(0., 540., 720.).looking_at(Vec3::new(0., 0., 267.), Vec3::Y);
+    let camera_transform = Transform::from_xyz(0., 540.*30., 720.*30.).looking_at(Vec3::new(0., 0., 267.), Vec3::Y);
     let camera_rotation = camera_transform.rotation.to_euler(EulerRot::YXZ);
 
     commands.spawn((
-        Camera3dBundle {
-            transform: Transform::from_xyz(0.7, 0.7, 1.0).looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
-            ..default()
-        },
+        Camera3dBundle::default(),
         orbit_camera::OrbitCamera {
             gimbal_x: -camera_rotation.0,
             gimbal_y: -camera_rotation.1,
@@ -78,11 +160,69 @@ fn spawn_environment(mut commands: Commands) {
     });
 }
 
-fn spawn_scene(mut commands: Commands, asset_server: Res<AssetServer>, map_file: Res<MapFile>) {
-    commands.spawn(SceneBundle {
-        scene: asset_server.load(format!("{}#Scene0", map_file.0)),
-        ..default()
-    });
+fn spawn_scene(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    map_file: Res<MapFile>,
+    mut images: ResMut<Assets<Image>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // let random_from_time = SystemTime::now()
+    //     .duration_since(SystemTime::UNIX_EPOCH)
+    //     .unwrap()
+    //     .as_millis() as u32;
+    // let hasher = PermutationTable::new(1);
+
+    // let mut buffer = vec![];
+    // let size = 1024;
+
+    // let mut min = f64::MAX;
+    // let mut max = f64::MIN;
+
+    // for i in 0..size {
+    //     for j in 0..size {
+    //         let x = i as f64 / size as f64 * 2. - 1.;
+    //         let y = j as f64 / size as f64 * 2. - 1.;
+
+    //         let color = open_simplex_2d(noise::Vector2::new(x * 10., y * 10.), &hasher);
+
+    //         min = min.min(color);
+    //         max = max.max(color);
+
+    //         let color = ((color / 1.2 + 0.5) * 256.) as u8;
+    //         // let color = 128;
+
+    //         buffer.push(color);
+    //         buffer.push(color);
+    //         buffer.push(color);
+    //         buffer.push(color);
+    //     }
+    // }
+    // dbg!(min, max);
+
+    // let image = images.add(Image::new(
+    //     Extent3d {
+    //         width: size,
+    //         height: size,
+    //         depth_or_array_layers: 1,
+    //     },
+    //     TextureDimension::D2,
+    //     buffer,
+    //     TextureFormat::Rgba8Unorm,
+    //     RenderAssetUsages::all(),
+    // ));
+
+    // commands.spawn(PbrBundle {
+    //     mesh: meshes.add(Plane3d::new(Vec3::Z).mesh().size(102400.0, 102400.0)),
+    //     material: materials.add(image),
+    //     ..default()
+    // });
+
+    // commands.spawn(SceneBundle {
+    //     scene: asset_server.load(format!("{}#Scene0", map_file.0)),
+    //     ..default()
+    // });
 }
 
 #[derive(Resource, Default)]
